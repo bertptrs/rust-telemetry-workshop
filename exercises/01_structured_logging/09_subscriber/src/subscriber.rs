18328a32ca6f8cf2b62508d84a870445b160d6f1
Bert Peters <bert@bertptrs.nl>
2024-05-08 11:39:58 +0200

    Very messy don't look

--- exercises/01_structured_logging/09_subscriber/src/subscriber.rs
+++ exercises/01_structured_logging/09_subscriber/src/subscriber.rs
@@ -1,5 +1,65 @@
 use helpers::MockWriter;
+use opentelemetry::KeyValue;
+use opentelemetry_otlp::WithExportConfig;
+use opentelemetry_sdk::runtime;
+use opentelemetry_sdk::trace::Tracer;
+use opentelemetry_sdk::Resource;
+use tonic::metadata::MetadataMap;
+use tracing::level_filters::LevelFilter;
+use tracing_subscriber::fmt::format::FmtSpan;
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::util::SubscriberInitExt;
+use tracing_subscriber::Layer;
+use tracing_subscriber::Registry;
 
 pub fn init_test_subscriber() -> MockWriter {
-    todo!()
+    let writer = MockWriter::new();
+    let writer2 = writer.clone();
+
+    let otel = tracing_opentelemetry::layer().with_tracer(init_otel());
+    let json = tracing_subscriber::fmt::layer()
+        .without_time()
+        .with_writer(move || writer.clone())
+        .with_ansi(false)
+        .with_level(false)
+        .with_target(false)
+        .with_span_events(FmtSpan::NEW | FmtSpan::EXIT)
+        .json()
+        .flatten_event(true)
+        .with_filter(LevelFilter::INFO);
+
+    Registry::default().with(json).with(otel).init();
+
+    writer2
+}
+
+fn init_otel() -> Tracer {
+    let honeycomb_key =
+        std::env::var("HONEYCOMB_API_KEY").expect("`HONEYCOMB_API_KEY` must be set");
+    let mut map = MetadataMap::with_capacity(1);
+    map.insert("x-honeycomb-team", honeycomb_key.try_into().unwrap());
+
+    // Correctly configuring your exporter is a bit of a black art and highly-dependent on the
+    // specifics of your deployment environment.
+    // We won't go into the details here, but you can read more about it in the OpenTelemetry
+    // documentation (or grab me after the workshop to talk about it).
+    // At a super high-level: you want batching and you want a sensible sampling strategy,
+    // but beyond that it's hard to give general advice.
+    opentelemetry_otlp::new_pipeline()
+        .tracing()
+        .with_trace_config(opentelemetry_sdk::trace::Config::default().with_resource(
+            Resource::new(vec![KeyValue::new(
+                "service.name",
+                "rust-telemetry-workshop",
+            )]),
+        ))
+        .with_exporter(
+            opentelemetry_otlp::new_exporter()
+                .tonic()
+                .with_endpoint("https://api.honeycomb.io/api/traces")
+                .with_timeout(std::time::Duration::from_secs(5))
+                .with_metadata(map),
+        )
+        .install_batch(runtime::Tokio)
+        .unwrap()
 }
